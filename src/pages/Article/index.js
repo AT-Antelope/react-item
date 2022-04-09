23fc54a1d7c044a8af88349090e600d107c2d67f
AT <q263629656@gmail.com>
2022-04-09 09:02:30 +0800

    因公共接口问题，项目不再更新

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,21 +1,139 @@
+import img404 from "@/assets/error.png";
 import "./index.scss";
 import { Link } from "react-router-dom";
+// import { observer } from "mobx-react-lite";
 import {
+  Table,
+  // Tag,
+  Space,
   Card,
   Breadcrumb,
-  DatePicker,
-  Button,
   Form,
+  Button,
   Radio,
+  DatePicker,
   Select,
 } from "antd";
-import locale from "antd/lib/date-picker/locale/zh_CN";
+import "moment/locale/zh-cn";
+import locale from "antd/es/date-picker/locale/zh_CN";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { useEffect, useState } from "react";
+// import { http } from "@/utils";
+// import { useStore } from "@/store";
 
 const { Option } = Select;
 const { RangePicker } = DatePicker;
 
 const Article = () => {
-  const onFinish = () => {};
+  const [channelList, setChannelList] = useState([]);
+
+  // Card数据
+  const data = [
+    {
+      id: "8218",
+      comment_count: 0,
+      cover: {
+        images: ["http://geek.itheima.net/resources/images/15.jpg"],
+      },
+      like_count: 0,
+      pubdate: "2019-03-11 09:00:00",
+      read_count: 2,
+      status: 2,
+      title: "webview离线化记载h5资源解决方案",
+    },
+  ];
+
+  const columns = [
+    {
+      title: "封面",
+      dataIndex: "cover",
+      width: 120,
+      render: (cover) => {
+        return (
+          <img src={cover.images[0] || img404} width={80} height={60} alt="" />
+        );
+      },
+    },
+    {
+      title: "标题",
+      dataIndex: "title",
+      width: 220,
+    },
+    {
+      title: "状态",
+      dataIndex: "status",
+      // render: (data) => formatStatus(data),
+    },
+    {
+      title: "发布时间",
+      dataIndex: "pubdate",
+    },
+    {
+      title: "阅读数",
+      dataIndex: "read_count",
+    },
+    {
+      title: "评论数",
+      dataIndex: "comment_count",
+    },
+    {
+      title: "点赞数",
+      dataIndex: "like_count",
+    },
+    {
+      title: "操作",
+      render: (data) => {
+        return (
+          <Space size="middle">
+            <Button
+              type="primary"
+              shape="circle"
+              icon={<EditOutlined />}
+              onClick={() => goPublish(data)}
+            />
+            <Button
+              type="primary"
+              danger
+              shape="circle"
+              icon={<DeleteOutlined />}
+              onClick={() => delArticle(data)}
+            />
+          </Space>
+        );
+      },
+      fixed: "right",
+    },
+  ];
+
+  // 文章列表统一管理
+  const [list, setList] = useState({
+    list: [], // 文章列表
+    count: 0, // 列表数量
+  });
+
+  // 文章参数管理
+  const [params, setParams] = useState({
+    page: 1, // 当前页数
+    per_page: 10, // 每页显示个数
+  });
+
+  const loadChannelList = async () => {
+    // const res = await http.get("/channels");
+    // console.log(res);
+    // setChannelList(res.data.channels);
+  };
+
+  // 提交表单
+  const onFinish = (value) => {
+    console.log(value);
+  };
+
+  const goPublish = (data) => {};
+  const delArticle = (data) => {};
+
+  useEffect(() => {
+    loadChannelList();
+  }, []);
 
   return (
     <div>
@@ -43,13 +161,12 @@ const Article = () => {
           </Form.Item>
 
           <Form.Item label="频道" name="channel_id">
-            <Select
-              placeholder="请选择文章频道"
-              defaultValue="lucy"
-              style={{ width: 120 }}
-            >
-              <Option value="jack">jack</Option>
-              <Option value="lucy">lucy</Option>
+            <Select placeholder="请选择文章频道" style={{ width: 120 }}>
+              {channelList.map((item) => (
+                <Option key={item.id} value={item.id}>
+                  {item.name}
+                </Option>
+              ))}
             </Select>
           </Form.Item>
 
@@ -66,20 +183,20 @@ const Article = () => {
         </Form>
       </Card>
       {/* 文章列表区域 */}
-      {/* <Card title={`根据筛选条件共查询到 ${articleData.count} 条结果：`}> */}
-      {/* <Table
+      <Card title={`根据筛选条件共查询到 articleData.count 条结果：`}>
+        <Table
           rowKey="id"
           columns={columns}
-          dataSource={articleData.list}
-          pagination={{
-            pageSize: params.per_page,
-            total: articleData.count,
-            onChange: pageChange,
-            current: params.page,
-          }}
+          // dataSource={articleData.list}
+          // pagination={{
+          //   pageSize: params.per_page,
+          //   total: articleData.count,
+          //   onChange: pageChange,
+          //   current: params.page,
+          // }}
           bordered
         />
-      </Card> */}
+      </Card>
     </div>
   );
 };
